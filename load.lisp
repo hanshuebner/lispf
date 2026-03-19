8cb936ff6d980b07469ea3603feeeb03cb754bc2
Hans Hübner <hans.huebner@gmail.com>
2026-03-19 21:47:04 +0100

    some pushing around on screens + better asdf initialization

--- load.lisp
+++ load.lisp
@@ -1,6 +1,14 @@
-(let ((here (make-pathname :defaults *load-truename* :name nil :type nil)))
-  (pushnew here asdf:*central-registry* :test #'equal)
-  (pushnew (merge-pathnames "CL3270/" here) asdf:*central-registry* :test #'equal)
-  (pushnew (merge-pathnames "screen-editor/" here) asdf:*central-registry* :test #'equal))
+(defun setup-registry (directory-path)
+  (format *error-output* "; adding components under ~A to asdf registry~%" directory-path)
+  (mapc (lambda (asd-pathname)
+          (pushnew (make-pathname :name nil
+                                  :type nil
+                                  :version nil
+                                  :defaults asd-pathname)
+                   asdf:*central-registry*
+                   :test #'equal))
+        (directory (merge-pathnames #p"**/*.asd" directory-path))))
+
+(setup-registry (make-pathname :defaults *load-truename* :name nil :type nil))
 
 (asdf:load-system "lispf")
